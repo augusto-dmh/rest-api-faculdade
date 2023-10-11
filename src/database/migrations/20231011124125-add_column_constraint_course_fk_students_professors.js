de2f524e76fef4b6022c1c56a63148c7fa527ddf
= <augustodemeloh@protonmail.com>
2023-10-11 14:05:46 -0300

    fix: linking students and professors to courses
    the tables were created but not related to each other. Now there's a relationship between the tables students/professors and the courses one.

---
+++ src/database/migrations/20231011124125-add_column_constraint_course_fk_students_professors.js
@@ -0,0 +1,42 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("students", "course_id", {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    });
+    await queryInterface.addColumn("professors", "course_id", {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    });
+
+    await queryInterface.addConstraint("students", {
+      type: "foreign key",
+      references: {
+        table: "courses",
+        field: "id",
+      },
+      fields: ["course_id"],
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+    });
+    await queryInterface.addConstraint("professors", {
+      type: "foreign key",
+      references: {
+        table: "courses",
+        field: "id",
+      },
+      fields: ["course_id"],
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.removeConstraint("students", "students_course_id_courses_fk");
+    await queryInterface.removeConstraint("professors", "professors_course_id_courses_fk");
+
+    await queryInterface.sequelize.query("ALTER TABLE `students` DROP COLUMN `course_id`");
+    await queryInterface.sequelize.query("ALTER TABLE `professors` DROP COLUMN `course_id`");
+  },
+};
