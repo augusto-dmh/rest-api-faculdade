57d5c6fff1198ca74750173c76257eff4b25e4b4
= <augustodemeloh@protonmail.com>
2023-10-15 13:57:22 -0300

    fix: db/models issues on connection and init
    The models weren't working in virtue of the index.js file - the entry point for all models -, which was empty yet. Also, the db was not mentioned on app.js - db connection wasn't working too.

---
+++ src/models/index.js
@@ -0,0 +1,10 @@
+import Course from "./Course";
+import Permission from "./Permission";
+import Professor from "./Professor";
+import Student from "./Student";
+import User from "./User";
+import UserPermission from "./UserPermission";
+
+const models = [Course, Permission, Professor, Student, User, UserPermission];
+
+export default models;
