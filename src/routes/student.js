cfe85ec37bbcefaeb77fdd09bf0327c703244fdc
= <augustodemeloh@protonmail.com>
2023-10-30 23:27:03 -0300

    refac: student routes using :ra instead of :id

--- src/routes/student.js
+++ src/routes/student.js
@@ -5,8 +5,8 @@ const router = new Router();
 
 router.post("/students", student.store);
 router.get("/students", student.index);
-router.get("/students/:id", student.show);
-router.put("/students/:id", student.update);
-router.delete("/students/:id", student.destroy);
+router.get("/students/:ra", student.show);
+router.put("/students/:ra", student.update);
+router.delete("/students/:ra", student.destroy);
 
 export default router;
