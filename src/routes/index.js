2d9b3c928fcff613b6a0c9ab0fe8c6e134e7dc65
= <augustodemeloh@protonmail.com>
2023-11-22 11:44:13 -0300

    refac: remove unnecessary code on routes/index.js
    on the index file of the routes folder (the file to re-export all modules from the folder), considering that what have to be exported by the file are the routes to be used on "app.use()" and the "use" method accepts an array of middlewares as the callback argument, and the Router instances exported by each module re-exported by the file are all middlewares, it's totally unnecessary to instantiate another router to use all the other routers: just exporting the array of routers is enough.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,3 @@
-import { Router } from "express";
-
 import home from "./home";
 import course from "./course";
 import modality from "./modality";
@@ -7,11 +5,8 @@ import student from "./student";
 import professor from "./professor";
 import permission from "./permission";
 import user from "./user";
+import login from "./login";
 
-const router = new Router();
-
-const routes = [home, course, modality, student, professor, permission, user];
-
-routes.forEach((route) => router.use(route));
+const routes = [home, course, modality, student, professor, permission, user, login];
 
 export default routes;
